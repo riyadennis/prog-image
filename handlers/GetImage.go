2f14593e10ddc533012371b320b444ac65b1eaef
Riya Dennis <riyacv@gmail.com>
2018-05-22 22:53:12 +0100

    Refactored image convertion function

--- handlers/GetImage.go
+++ handlers/GetImage.go
@@ -13,8 +13,16 @@ import (
 	"image/png"
 	"github.com/sirupsen/logrus"
 	"image"
-	_ "image/jpeg"
+	"image/jpeg"
+	"image/gif"
 )
+
+type FileConverter struct {
+	NewFileType string
+	OldFileType string
+	NewFile     io.Writer
+	OldFile     io.Reader
+}
 
 func GetImageHandler(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
 	requestImageId := params.ByName("image_id")
@@ -36,7 +44,7 @@ func GetImageHandler(w http.ResponseWriter, req *http.Request, params httprouter
 	//detail for get requests with out conversion
 	detail := fmt.Sprintf("Image URL: http://localhost:8080/%s", imageName)
 	if requestImageType != "" {
-		err = CreateNewImageForImageType(requestImageId,requestImageType, imageType, config)
+		err = CreateNewImageForImageType(requestImageId, requestImageType, imageType, config)
 		if err != nil {
 			// wrong image format and not able to create new image
 			http.Error(w, "Unable to process the request", http.StatusBadRequest)
@@ -58,25 +66,38 @@ func CreateNewImageForImageType(requestImageId, requestImageType, existingImageT
 	oldFilePath := fmt.Sprintf("%s/%s.%s", config.Prog.Folder, requestImageId, existingImageType)
 	oldFile, err := os.Open(oldFilePath)
 	if err != nil {
-		return  err
+		return err
 	}
 	newFile, err := os.Create(newFilePath)
 	if err != nil {
 		return err
 	}
-	if requestImageType == "png" {
-		err := convertToPNG(newFile, oldFile)
-		if err != nil {
-			return err
-		}
+	fileConverter := FileConverter{
+		NewFileType: requestImageType,
+		NewFile:newFile,
+		OldFileType: existingImageType,
+		OldFile: oldFile,
 	}
-
-	return nil
-}
-func convertToPNG(newFile io.Writer, oldFile io.Reader) error {
-	img, _, err := image.Decode(oldFile)
+	err = fileConverter.ConvertFileFromOnTypeToAnother()
 	if err != nil {
 		return err
 	}
-	return png.Encode(newFile, img)
+	return nil
+}
+func (fc FileConverter) ConvertFileFromOnTypeToAnother() error {
+	img, _, err := image.Decode(fc.OldFile)
+	if err != nil {
+		return err
+	}
+	switch fc.NewFileType {
+	case "jpg":
+		return jpeg.Encode(fc.NewFile, img, nil)
+	case "png" :
+		return png.Encode(fc.NewFile, img)
+	case "gif":
+		return gif.Encode(fc.NewFile, img, nil)
+	default:
+		return errors.New("Invalid file format")
+	}
+	return nil
 }
--- handlers/GetImageHandler_test.go
+++ handlers/GetImageHandler_test.go
@@ -7,7 +7,9 @@ import (
 	"os"
 	"fmt"
 )
+
 const testImage = "testfile"
+
 func TestCreateNewImageForInvalidImageType(t *testing.T) {
 	config := middleware.Config{}
 	err := CreateNewImageForImageType(testImage, "invaliderr", "png", &config)
@@ -19,15 +21,63 @@ func TestCreateNewImageForValidImageTypeWithInvalidOldFile(t *testing.T) {
 	allowedFileTypes[0] = "png"
 	allowedFileTypes[1] = "jpg"
 	prog := middleware.Prog{
-		Folder: "../images",
-		FileType:allowedFileTypes,
+		Folder:   "../images",
+		FileType: allowedFileTypes,
 	}
 	config := middleware.Config{
-		Prog:prog,
+		Prog: prog,
 	}
 	newFormat := "png"
 	newFilePath := fmt.Sprintf("%s/%s.%s", prog.Folder, testImage, newFormat)
 	os.Remove(newFilePath)
 	err := CreateNewImageForImageType(testImage, newFormat, "jpg", &config)
 	assert.NoError(t, err)
+}
+func TestFileConverter_ConvertFileFromJpgToPng(t *testing.T) {
+	newFileType := "png"
+	newFile, _ := os.Create("testfile.png")
+	oldFileType := "jpg"
+	oldFIlePath := fmt.Sprintf("../images/%s.%s", testImage, oldFileType)
+	oldFile, _ := os.Open(oldFIlePath)
+	fc := FileConverter{
+		NewFileType: newFileType,
+		NewFile:     newFile,
+		OldFileType: oldFileType,
+		OldFile:     oldFile,
+	}
+	err := fc.ConvertFileFromOnTypeToAnother()
+	os.Remove("testfile.png")
+	assert.NoError(t, err)
+}
+func TestFileConverter_ConvertFileFromPngToGif(t *testing.T) {
+	newFileType := "gif"
+	newFile, _ := os.Create("testfile.gif")
+	oldFileType := "png"
+	oldFIlePath := fmt.Sprintf("../images/%s.%s", testImage, oldFileType)
+	oldFile, _ := os.Open(oldFIlePath)
+	fc := FileConverter{
+		NewFileType: newFileType,
+		NewFile:     newFile,
+		OldFileType: oldFileType,
+		OldFile:     oldFile,
+	}
+	err := fc.ConvertFileFromOnTypeToAnother()
+	os.Remove("testfile.gif")
+	assert.NoError(t, err)
+}
+func TestFileConverter_ConvertFileFromPngToInvalid(t *testing.T) {
+	newFileType := "jpegs"
+	newFile, _ := os.Create("testfile.jpegs")
+	oldFileType := "png"
+	oldFIlePath := fmt.Sprintf("../images/%s.%s", testImage, oldFileType)
+	oldFile, _ := os.Open(oldFIlePath)
+	fc := FileConverter{
+		NewFileType: newFileType,
+		NewFile:     newFile,
+		OldFileType: oldFileType,
+		OldFile:     oldFile,
+	}
+	err := fc.ConvertFileFromOnTypeToAnother()
+	os.Remove("testfile.jpegs")
+	assert.Error(t, err)
 }
